ebd43fb67796e3d603cf181f6358b31005715743
RGTECH <rg.techprojects@gmail.com>
2026-04-03 23:36:29 +0530

    Update settings.py

--- proj_fd/settings.py
+++ proj_fd/settings.py
@@ -141,6 +141,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'assets/static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
